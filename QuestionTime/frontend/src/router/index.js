44fcd1449f59abdfca3c78fc68cfb5e05ded22c8
Nick Lofaro <5376991+nickoralias@users.noreply.github.com>
2020-06-12 13:50:58 -0700

    Add 404 error view

--- QuestionTime/frontend/src/router/index.js
+++ QuestionTime/frontend/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import AnswerEditor from "@/views/AnswerEditor.vue";
 import Home from "@/views/Home.vue";
+import NotFound from "@/views/NotFound.vue";
 import Question from "@/views/Question.vue";
 import QuestionEditor from "@/views/QuestionEditor.vue";
 
@@ -30,6 +31,11 @@ const routes = [
     name: "answer-editor",
     component: AnswerEditor,
     props: true
+  },
+  {
+    path: '*',
+    name: 'page-not-found',
+    component: NotFound
   }
 ];
 
